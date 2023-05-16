00eebefbbd72d0aad979885606f83cdd43cbbc4e
amin <maminhs8324@gmai.com>
2023-05-16 19:08:57 +0330

    public static void Main(string[] args)

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System ;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static List<Human> StudentFile()
+        {
+            dynamic StudentJsonFile =
+                JsonConvert.DeserializeObject<List<Human>>(File.ReadAllText("JSON_DATA.json"));
+            return StudentJsonFile;
+        }
+        public static void MainPage()
+        {
+            Console.WriteLine("********Wellcome To Hogwars************ ");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Sign In As Dumbledore (D)");
+            Console.WriteLine("Sign In As Ticher (T)");
+            Console.WriteLine("Sign In As Student (S)");
+            Console.ReadLine();
+
+
+
+        }
+        
+        
+        
+        public static void Main(string[] args)
+        {
+            StudentFile();
+            Console.ReadKey();
+
+        }
+        
+    }
+}
---
+++ ConsoleApplication1/human.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApplication1
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public int DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Father { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string RaceType { get; set; }
+        public string Role { get; set; }
+    }
+}
